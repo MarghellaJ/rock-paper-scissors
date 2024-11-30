5305d7cecff0c84d3ec8b0bd551f2f4c7fd3b09a
Jim Marghella <j.marghella@gmail.com>
2024-11-30 18:11:52 -0500

    game play
    working on gamePlay() to make game repeat n time.

--- script.js
+++ script.js
@@ -1,24 +1,80 @@
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3); //generates random number
 
-    switch(randomNumber) { //return the String associated with randomNumber
-        case 0:
-            return "scissors";
-        case 1:
-            return "rock";
-        case 2:
-            return "paper";
-        default:
-            return "Error";
-    }    
+    if (randomNumber === 0) {
+        return "rock";
+    } 
+    if ( randomNumber === 1) {
+        return "paper";
+    } 
+    else {
+        return "scissors";
+    }
 }
 
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let choice = prompt("Choose rock, paper, or scissors");
-    console.log(choice);
+    let choice = prompt("Choose rock, paper, or scissors").toLowerCase(); //prompt window asking for user input
+
+    if (choice === "rock") {
+        return "rock";
+    }
+    if (choice === "paper") {
+        return "paper";
+    }
+    if (choice === "scissors") {
+        return "scissors";
+    }
+    else {
+        return 
+    }
 }
 
-getHumanChoice();
+
+
+
+
+
+
+
+function playGame() {
+    while (humanScore <= 5 && computerScore <= 5) {
+        function playRound(humanChoice, computerChoice) {
+            humanChoice = humanChoice.toLowerCase()
+            if (humanChoice === computerChoice) {
+                alert('It\'s a TIE!');
+                alert('You both lose...');
+                alert(`${humanChoice} is equal to ${computerChoice}`);
+                alert(`Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
+            }
+        
+            if (humanChoice != computerChoice) {
+                if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+                    humanScore++
+                    alert('You win!');
+                    alert(`Computer chose ${computerChoice}`);
+                    alert(`${humanChoice} beats ${computerChoice}...`)
+                    alert(`Good Job!`);
+                    alert(` Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
+        
+                } 
+                else {
+                    computerScore++
+                    alert('Computer wins!');
+                    alert(`Computer chose ${computerChoice}`)
+                    alert(` ${computerChoice} beats ${humanChoice}...`);
+                    alert('What a loser!');
+                    alert(` Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
+                }
+            }
+            
+        }
+    }
+    playRound(humanSelection, computerSelection);
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
