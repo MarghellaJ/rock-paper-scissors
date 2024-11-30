be5e9957b3ecb9829e7d3c0f970104d4088818ab
Jim Marghella <j.marghella@gmail.com>
2024-11-30 18:48:58 -0500

    playGame
    I have the game playing a single round and need to now make it play
    for n rounds before determining the overall winner.

--- script.js
+++ script.js
@@ -34,47 +34,55 @@ function getHumanChoice() {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    if (humanChoice === computerChoice) { // check for tie
+        alert('It\'s a TIE!');
+        alert('You both lose...');
+        alert(`${humanChoice} is equal to ${computerChoice}`);
+        alert(`Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
+    }
 
-
-
-
-
-
-
-function playGame() {
-    while (humanScore <= 5 && computerScore <= 5) {
-        function playRound(humanChoice, computerChoice) {
-            humanChoice = humanChoice.toLowerCase()
-            if (humanChoice === computerChoice) {
-                alert('It\'s a TIE!');
-                alert('You both lose...');
-                alert(`${humanChoice} is equal to ${computerChoice}`);
-                alert(`Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
-            }
-        
-            if (humanChoice != computerChoice) {
-                if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
-                    humanScore++
-                    alert('You win!');
-                    alert(`Computer chose ${computerChoice}`);
-                    alert(`${humanChoice} beats ${computerChoice}...`)
-                    alert(`Good Job!`);
-                    alert(` Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
-        
-                } 
-                else {
-                    computerScore++
-                    alert('Computer wins!');
-                    alert(`Computer chose ${computerChoice}`)
-                    alert(` ${computerChoice} beats ${humanChoice}...`);
-                    alert('What a loser!');
-                    alert(` Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
-                }
-            }
+    if (humanChoice != computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+            
+            alert('You win!');
+            alert(`Computer chose ${computerChoice}`);
+            alert(`${humanChoice} beats ${computerChoice}...`)
+            alert(`Good Job!`);
+            humanScore++
+            alert(` Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
+            
+        } 
+        else {
+            alert('Computer wins!');
+            alert(`Computer chose ${computerChoice}`)
+            alert(` ${computerChoice} beats ${humanChoice}...`);
+            alert('What a loser!');
+            computerScore++;
+            alert(` Player Score: ${humanScore}\n Computer Score: ${computerScore}`);
             
         }
+        
     }
-    playRound(humanSelection, computerSelection);
+    
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    
+    playRound(humanSelection, computerSelection);
+
+
+
+
+// function playGame() {
+//     humanScore = 0;
+//     computerScore = 0;
+    
+//     for (let i = 1; i <=5; i++) {
+        
+//         console.log(`Round ${i}`)
+//         playRound();
+//         console.log(`Score: You - ${humanScore}, Computer - ${computerScore}`);
+//     }
+// }
